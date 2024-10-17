19d69d654d5d2df3bd99ef7ce79e1a641d4eab86
fschoppel <fhs48043@fh-salzburg.ac.at>
2024-10-17 20:54:06 +0200

    activated reload-prevention

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function closeIt()
   return "Any string value here forces a dialog box to \n" + 
          "appear before closing the window.";
 }
-//window.onbeforeunload = closeIt;
+window.onbeforeunload = closeIt;
 
 //Functions for Buttons
 function checklistMenuListButton(_checklistNum) {
